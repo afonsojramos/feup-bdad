e3abcafd8b4f522f2e103ffb9de7f2bc9eae58a5
Afonso Jorge Ramos <up201506239@fe.up.pt>
2017-04-02 00:55:05 +0100

    FIXED EVERY ERROR

--- create.sql
+++ create.sql
@@ -3,9 +3,6 @@
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
 
-drop table if exists PremiumUser;
-drop table if exists FreeUser;
-drop table if exists User;
 drop table if exists Watched;
 drop table if exists Comment;
 drop table if exists Message;
@@ -15,14 +12,36 @@ drop table if exists Episode;
 drop table if exists Season;
 drop table if exists Show;
 drop table if exists Country;
+drop table if exists PremiumUser;
+drop table if exists FreeUser;
+drop table if exists User;
 
+create table User(
+    idUser INTEGER NOT NULL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    birthday DATE NOT NULL,
+    gender varchar(6) CHECK (gender="Male" or gender="Female"),
+    idCountry INTEGER NOT NULL REFERENCES Country(idCountry) ON DELETE CASCADE,
+    userType INTEGER CHECK (userType=0 or userType=1)
+);
+
+create table FreeUser(
+    idUser INTEGER NOT NULL REFERENCES User(idUser) ON DELETE CASCADE PRIMARY KEY
+);
+
+create table PremiumUser(
+    idUser INTEGER NOT NULL REFERENCES User(idUser) ON DELETE CASCADE PRIMARY KEY,
+    subscriptionFee INTEGER DEFAULT 1,
+    noAds INTEGER DEFAULT 1,
+    rewatchEpisodes INTEGER DEFAULT 1
+);
 create table Country(
-    idCountry INTEGER PRIMARY KEY,
+    idCountry INTEGER NOT NULL PRIMARY KEY,
     name TEXT NOT NULL
 );
 
 create table Show(
-    idShow INTEGER PRIMARY KEY,
+    idShow INTEGER NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     usualAirtime TIME NOT NULL,
     network TEXT NOT NULL,
@@ -31,32 +50,32 @@ create table Show(
 );
 
 create table Season(
-    idSeason INTEGER PRIMARY KEY,
+    idSeason INTEGER NOT NULL PRIMARY KEY,
     nSeason INTEGER NOT NULL,
     idShow INTEGER NOT NULL REFERENCES Show(idShow)
 );
 
 create table Episode(
-    idEpisode INTEGER PRIMARY KEY,
+    idEpisode INTEGER NOT NULL PRIMARY KEY,
     epNumber INTEGER NOT NULL,
     releaseDate DATE NOT NULL,
     idSeason INTEGER NOT NULL REFERENCES Season(idSeason)
 );
 
 create table Actor(
-    idActor INTEGER PRIMARY KEY,
+    idActor INTEGER NOT NULL PRIMARY KEY,
     name TEXT NOT NULL
 );
 
 create table Character(
-    idCharacter INTEGER PRIMARY KEY,
+    idCharacter INTEGER NOT NULL PRIMARY KEY,
     characterName TEXT NOT NULL,
     idActor INTEGER NOT NULL REFERENCES Actor(idActor),
     idShow INTEGER NOT NULL REFERENCES Show(idShow)
 );
 
 create table Message(
-   idMessage INTEGER PRIMARY KEY,
+   idMessage INTEGER NOT NULL PRIMARY KEY,
    content TEXT NOT NULL,
    msgDate DATE NOT NULL,
    idUser1 INTEGER NOT NULL REFERENCES User(idUser), 
@@ -64,7 +83,7 @@ create table Message(
 );
 
 create table Comment(
-   idComment INTEGER PRIMARY KEY,
+   idComment INTEGER NOT NULL PRIMARY KEY,
    content TEXT NOT NULL, 
    cmtDate DATE NOT NULL,
    idUser INTEGER NOT NULL REFERENCES User(idUser), 
@@ -79,22 +98,3 @@ create table Watched(
     CONSTRAINT Watched_pk PRIMARY KEY (idEpisode,idUser)
 );
 
-create table User(
-    idUser INTEGER PRIMARY KEY,
-    username TEXT UNIQUE NOT NULL,
-    birthday DATE NOT NULL,
-    gender varchar(6) CHECK (gender="Male" or gender="Female"),
-    idCountry INTEGER NOT NULL REFERENCES Country(idCountry) ON DELETE CASCADE,
-    userType INTEGER CHECK (userType=0 or userType=1)
-);
-
-create table FreeUser(
-    idUser INTEGER REFERENCES User(idUser) PRIMARY KEY
-);
-
-create table PremiumUser(
-    idUser INTEGER REFERENCES User(idUser) PRIMARY KEY,
-    subscriptionFee INTEGER DEFAULT 1,
-    noAds INTEGER DEFAULT 1,
-    rewatchEpisodes INTEGER DEFAULT 1
-);
