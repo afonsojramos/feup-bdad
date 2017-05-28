6a388e344f0154278f72e368690cd6db769ee039
zepedrob16 <up201503603@fe.up.pt>
2017-05-28 17:34:52 +0100

    Queries :heavy_check_mark:

--- query.sql
+++ query.sql
@@ -1,33 +1,33 @@
--- Listar utilizadores
-Select username
-From User;
+﻿-- Listar utilizadores
+SELECT username
+FROM User;
 
 -- listar series por rating
-Select name
-From Show
-order by rating
-ASC;
+SELECT name AS Name, rating AS Rating
+FROM Show
+ORDER BY rating
+DESC;
 
 -- Series de animação que dão antes das 8 da manhã
-Select name
-From Show
-where (airtime < 0800 AND genre like '%animation%');
+SELECT name
+FROM Show
+WHERE (Show.usualAirtime < 0800 AND genre LIKE '%animation%');
 
 -- Comentários deixados por user premium
-Select content
-From Comment
+SELECT content
+FROM Comment
 INNER JOIN PremiumUser
 ON Comment.idUser = PremiumUser.idUser;
 
 -- Comentários deixados no mesmo dia em que o episódio foi lançado
-Select *
-From Comment
+SELECT *
+FROM Comment
 INNER JOIN Episode
 ON (Comment.cmtDate = Episode.releaseDate AND Comment.idEpisode = Episode.idEpisode);
 
 -- Países ordenados por numero de utilizadores
-Select Country.name AS Name, COUNT(User.idCountry) AS NumberOfUsers
-From Country
+SELECT Country.name AS Name, COUNT(User.idCountry) AS NumberOfUsers
+FROM Country
 INNER JOIN User
 ON User.idCountry = Country.idCountry
 GROUP BY Country.name
@@ -35,36 +35,35 @@ ORDER BY NumberOfUsers
 DESC;
 
 -- Utilizadores com mais mensagens enviadas no verão de 2012
-Select User.username AS user, Count(Message.idUser1) AS NumberOfMessages
+SELECT User.username AS user, Count(Message.idUser1) AS NumberOfMessages
 FROM User
 INNER JOIN Message
 ON Message.idUser1 = User.idUser
-Where strftime('%Y-%m-%d', '2012-06-21') < strftime('%Y-%m-%d', Message.msgDate) AND strftime('%Y-%m-%d', Message.msgDate) < strftime('%Y-%m-%d', '2012-09-22')
+WHERE strftime('%Y-%m-%d', '2012-06-21') < strftime('%Y-%m-%d', Message.msgDate) AND strftime('%Y-%m-%d', Message.msgDate) < strftime('%Y-%m-%d', '2012-09-22')
 GROUP BY User.username
 ORDER BY NumberOfMessages
 DESC;
 
--- Atores que interpretam mais que uma personagem
-Select Actor.name AS Name, COUNT(Character.idActor) AS NumberOfCharacters
-From Actor
+-- Numero de personagens interpretadas por cada ator
+SELECT Actor.name AS Name, COUNT(Character.idActor)AS NumberOfCharacters
+FROM Actor
 INNER JOIN Character
 ON Actor.idActor = Character.idActor
-Where NumberOfCharacters > 1
 GROUP BY Actor.name
 ORDER BY NumberOfCharacters
 DESC;
 
 -- Networks com maior rating
-Select Show.network AS Name, AVG(rating) AS AverageRating
-From Show
-Where Show.network = Show.network
+SELECT Show.network AS Name, AVG(rating) AS AverageRating
+FROM Show
+WHERE Show.network = Show.network
 GROUP BY Show.network
 ORDER BY AverageRating
 DESC;
 
 --Idade média dos utilizadores que seguem Game Of Thrones
-Select AVG(strftime('%Y-%m-%d', 'now') - strftime('%Y-%m-%d', User.birthday)) AS Age
-From User
+SELECT AVG(strftime('%Y-%m-%d', 'now') - strftime('%Y-%m-%d', User.birthday)) AS Age
+FROM User
 INNER JOIN Watched
 ON Watched.idUser = User.idUser
 INNER JOIN Episode
@@ -73,3 +72,24 @@ INNER JOIN Season
 ON Season.idSeason = Episode.idSeason
 INNER JOIN Show
 ON Season.idShow = 1;
+
+
+--Séries recomendadas a quem vê séries de comédia
+SELECT Show.name AS Name, Show.rating AS rating
+FROM Show
+WHERE genre LIKE '%Comedy%'
+ORDER BY Show.rating
+DESC;
+
+-- Séries vistas pelo User com id 6
+SELECT DISTINCT Show.name AS Name, Show.rating AS Rating
+FROM Show, User
+INNER JOIN Watched
+ON Watched.idUser = User.idUser AND User.idUser = 14
+INNER JOIN Episode
+ON Episode.idEpisode = Watched.idEpisode
+INNER JOIN Season
+ON Season.idSeason = Episode.idSeason
+Where Season.idShow = Show.idShow
+ORDER BY Rating
+DESC;
